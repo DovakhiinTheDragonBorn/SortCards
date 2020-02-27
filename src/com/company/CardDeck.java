d65bfd5460030b412b6121f5103f76fed4af2536
Joe Mascarenhas <joe.mascarenhas1902@gmail.com>
2020-02-28 00:31:02 +0530

    Added Menu

--- src/com/company/CardDeck.java
+++ src/com/company/CardDeck.java
@@ -41,4 +41,10 @@ public class CardDeck {
             }
         }
     }
+
+    public void SortRows() {
+    }
+
+    public void SortColumns() {
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,42 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         CardDeck myDeck = new CardDeck();
-        //myDeck.ShowCards();
+        int i = -1;
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (i!=0)
+        {
+            System.out.println();
+            System.out.println("Please Select an Operation");
+            System.out.println("1. Shuffle Deck");
+            System.out.println("2. Show Cards");
+            System.out.println("3. Sort Rows");
+            System.out.println("4. Sort Columns");
+            System.out.println("0. Exit");
+            i = Integer.parseInt(scanner.next());
+            switch (i)
+            {
+                case 1: myDeck.Shuffle();
+                        break;
+                case 2:System.out.println();
+                        myDeck.ShowCards();
+                        break;
+                case 3:myDeck.SortRows();
+                        break;
+                case 4:myDeck.SortColumns();
+                        break;
+                case 0: break;
+                default: System.out.println("Please Enter a valid option");
+            }
+
+        }
+
         myDeck.Shuffle();
         myDeck.ShowCards();
     }
